01a30f55dd160d7e26796a428fd25afef9e2b9bb
yong <yong@hackle.io>
2024-05-07 14:31:22 +0900

    coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,13 @@
 # frozen_string_literal: true
 
 require 'simplecov'
+require 'simplecov-lcov'
+SimpleCov::Formatter::LcovFormatter.config.report_with_single_file = true
+SimpleCov.formatters = SimpleCov::Formatter::MultiFormatter.new([
+                                                                  SimpleCov::Formatter::HTMLFormatter,
+                                                                  SimpleCov::Formatter::LcovFormatter
+                                                                ])
+
 SimpleCov.start { add_filter '/spec/' } if ENV['COVERAGE']
 
 require 'hackle'
